4636fb2a343689bdb814d0e2b77febcbfd6dcd9d
Rospok <gabriel252318@gmail.com>
2026-06-27 03:28:11 -0400

    Add routes for admin, alert, and maps controllers
    - Created routes for the admin section including index, store, show, update, and destroy methods.
    - Added routes for alert management with index and store methods.
    - Introduced a route for accessing maps.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerAdmin;
+use App\Http\Controllers\ControllerAlert;
+use App\Http\Controllers\ControllerMaps;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/losbuenosomosmas/admin", [ControllerAdmin::class, "index"]);
+Route::post("/losbuenosomosmas/admin", [ControllerAdmin::class, "store"]);
+Route::get("/losbuenosomosmas/admin/{id}", [ControllerAdmin::class, "show"]);
+Route::put("/losbuenosomosmas/admin/{id}", [ControllerAdmin::class, "update"]);
+Route::delete("/losbuenosomosmas/admin/{id}", [ControllerAdmin::class, "destroy"]);
+Route::get("/losbuenosomosmas/alert", [ControllerAlert::class, "index"]);
+Route::post("/losbuenosomosmas/alert", [ControllerAlert::class, "store"]);
+Route::get("/losbuenosomosmas/maps", [ControllerMaps::class, "index"]);
